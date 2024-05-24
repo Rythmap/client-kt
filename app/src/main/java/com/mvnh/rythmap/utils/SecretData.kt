856edff6ddce2cd6722dd9a50de431ae1107329f
mvnh <dider90n2@gmail.com>
2024-05-25 00:47:40 +0300

    oopsie

--- app/src/main/java/com/mvnh/rythmap/utils/SecretData.kt
+++
@@ -1,7 +0,0 @@
-package com.mvnh.rythmap.utils
-
-object SecretData {
-    const val JAWG_ACCESS_TOKEN = "zoJPCAjHZjk9n2ZcHEij37TCrQY27K8FzYhq6wzaYBUmdV07RLTVyxTsfx5DqEXI"
-    const val SERVER_URL = "melomap.fun"
-    const val TAG = "Rythmap"
-}
